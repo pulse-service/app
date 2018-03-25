b5b3bd0416c3e87fbe65da341e96e553d1e813bd
Nirzon Pop <nirzon192@gmail.com>
2018-03-25 12:11:24 +0600

    Refactor the class

--- app/src/main/java/com/electrocraft/nirzo/pluse/controller/receive/DoctorIncomingAlertReceiver.java
+++ app/src/main/java/com/electrocraft/nirzo/pluse/controller/receive/DoctorIncomingAlertReceiver.java
@@ -4,21 +4,23 @@ package com.electrocraft.nirzo.pluse.controller.receive;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import com.electrocraft.nirzo.pluse.controller.network.NotifySMSReceived;
+import com.electrocraft.nirzo.pluse.view.activity.doctor.DocNotifyCallReceivedAlertActivity;
 
 
 
 
 /**
- * Created by nirzo on 3/17/2018.
+ *
+ * @author Faisal
+ * @since 3/17/2018
  */
 
-public class DoctorReceiver extends BroadcastReceiver {
-    private String TAG = "DoctorReceiver";
+public class DoctorIncomingAlertReceiver extends BroadcastReceiver {
+    private String TAG = "DoctorIncomingAlertReceiver";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent i = new Intent(context, NotifySMSReceived.class);
+        Intent i = new Intent(context, DocNotifyCallReceivedAlertActivity.class);
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(i);
 
